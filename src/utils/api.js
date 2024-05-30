04a87645f98ce8f2eb7892d01d7be7700e70fbb8
Soo Jung <comme746@naver.com>
2024-05-30 10:22:38 +0900

    api modify

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
 
 const api = axios.create({
   // baseURL: LOCAL_BACKEND,
-  baseURL: BACKEND_PROXY,
+  baseURL: `${BACKEND_PROXY}/api`,
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${sessionStorage.getItem("token")}`,
