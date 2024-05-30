90f80dc98c62d10357115f8d9e05608efd251db7
Soo Jung <comme746@naver.com>
2024-05-30 15:09:50 +0900

    4th commit

--- src/action/userAction.js
+++ src/action/userAction.js
@@ -2,25 +2,53 @@ import api from "../utils/api";
 import * as types from "../constants/user.constants";
 import { commonUiActions } from "./commonUiAction";
 import * as commonTypes from "../constants/commonUI.constants";
-const loginWithToken = () => async (dispatch) => {};
-const loginWithEmail = (payload) => async (dispatch) => {};
-const logout = () => async (dispatch) => {};
+const loginWithToken = () => async (dispatch) => {
+  try {
+    dispatch({type: types.TOKEN_LOGIN_REQUEST})
+    const res = await api.get('/user/me');
+    if(res.status !== 200) throw new Error('Invalid token');
+    dispatch({type: types.TOKEN_LOGIN_SUCCESS, payload: res.data})
+  } catch (error) {
+    dispatch({type: types.TOKEN_LOGIN_FAIL, payload: error.error})
+    dispatch(logout())
+  }
+};
+
+const loginWithEmail = 
+  ({ email, password }, navigate) => 
+    async (dispatch) => {
+      try {
+        dispatch({type: types.LOGIN_REQUEST})
+        const res = await api.post('/user/login', {email, password})
+        if(res.status !== 200) throw new Error(res.error)
+        dispatch({type: types.LOGIN_SUCCESS, payload: res.data})
+        sessionStorage.setItem('token', res.data.token)
+        dispatch(commonUiActions.showToastMessage("로그인을 완료했습니다.", "success"))
+      } catch (error) {
+        dispatch({type: types.LOGIN_FAIL, payload: error.error})
+      }
+  };
+
+const logout = () => async (dispatch) => {
+  dispatch({type: types.LOGOUT});
+  sessionStorage.removeItem('token');
+};
 
 const loginWithGoogle = (token) => async (dispatch) => {};
 
 const registerUser =
   ({ email, name, password }, navigate) =>
   async (dispatch) => {
-    try {
-      dispatch({type: types.REGISTER_USER_REQUEST})
-      const res = await api.post('/user', {email, name, password})
-      if(res.status !== 200) throw new Error(res.error)
-      dispatch({type: types.REGISTER_USER_SUCCESS})
-      dispatch(commonUiActions.showToastMessage("회원가입을 완료했습니다.", "success"))
-      navigate('/login')
-    } catch (error) {
-      dispatch({type: types.REGISTER_USER_FAIL, payload: error.error})
-    }
+      try {
+        dispatch({type: types.REGISTER_USER_REQUEST})
+        const res = await api.post('/user', {email, name, password})
+        if(res.status !== 200) throw new Error(res.error)
+        dispatch({type: types.REGISTER_USER_SUCCESS})
+        dispatch(commonUiActions.showToastMessage("회원가입을 완료했습니다.", "success"))
+        navigate('/login')
+      } catch (error) {
+        dispatch({type: types.REGISTER_USER_FAIL, payload: error.error})
+      }
   };
 export const userActions = {
   loginWithToken,
--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -1,9 +1,32 @@
 import * as types from "../constants/user.constants";
-const initialState = {};
+const initialState = {
+  loading: false,
+  user: null,
+  error: ''
+};
 
 function userReducer(state = initialState, action) {
   const { type, payload } = action;
-  return state;
+  switch(type) {
+    case types.REGISTER_USER_REQUEST:
+    case types.LOGIN_REQUEST:
+    case types.TOKEN_LOGIN_REQUEST:
+      return {...state, loading: true}
+    case types.LOGIN_SUCCESS:
+    case types.TOKEN_LOGIN_SUCCESS:
+      return {...state, loading: false, user:payload.user}
+    case types.REGISTER_USER_SUCCESS:
+      return {...state, loading: false}
+    case types.LOGIN_FAIL:
+    case types.REGISTER_USER_FAIL:
+      return {...state, loading: false, error: payload};
+    case types.TOKEN_LOGIN_FAIL:
+      return {...state, loading: false};
+    case types.LOGOUT:
+      return {...state, user: null}
+    default:
+      return state;
+  }
 }
 
 export default userReducer;
