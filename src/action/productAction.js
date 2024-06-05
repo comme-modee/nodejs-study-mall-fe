61d9fe37d93c7f99cf402ae7aae7761349990c41
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-05 21:26:08 +0900

    Update productAction.js

--- src/action/productAction.js
+++ src/action/productAction.js
@@ -6,7 +6,18 @@ import { commonUiActions } from "./commonUiAction";
 const getProductList = (query) => async (dispatch) => {};
 const getProductDetail = (id) => async (dispatch) => {};
 
-const createProduct = (formData) => async (dispatch) => {};
+const createProduct = (formData) => async (dispatch) => {
+  try {
+    dispatch({type:types.PRODUCT_CREATE_REQUEST});
+    const res = await api.post('/product', formData);
+    if(res !== 200) throw new Error(res.error);
+    dispatch({type:types.PRODUCT_CREATE_SUCCESS});
+    dispatch(commonUiActions.showToastMessage('상품 생성 완료', 'success'));
+  } catch (error) {
+    dispatch({type:types.PRODUCT_CREATE_FAIL, payload: error.error});
+    dispatch(commonUiActions.showToastMessage(error.error, 'error'));
+  }
+};
 const deleteProduct = (id) => async (dispatch) => {};
 
 const editProduct = (formData, id) => async (dispatch) => {};
