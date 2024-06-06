9d310c646881f584f2ebb388a73ee2dad8342007
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-06 20:07:44 +0900

    Update ProductAll.js

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import ProductCard from "../component/ProductCard";
 import { Row, Col, Container } from "react-bootstrap";
-import { useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { productActions } from "../action/productAction";
 import { commonUiActions } from "../action/commonUiAction";
@@ -9,17 +8,12 @@ import { commonUiActions } from "../action/commonUiAction";
 const ProductAll = () => {
   const dispatch = useDispatch();
   const { productList, totalPage } = useSelector((state) => state.product);
-  const [ query, setQuery ] = useSearchParams();
-  const [ searchQuery, setSearchQuery ] = useState({
-    page: query.get("page") || 1,
-    name: query.get("name") || "",
-  }); //검색 조건들을 저장하는 객체
   const error = useSelector((state) => state.product.error);
     
   // 처음 로딩하면 상품리스트 불러오기
   useEffect(()=>{
-    dispatch(productActions.getProductList({ ...searchQuery }))
-  },[query])
+    dispatch(productActions.getProductList())
+  },[])
 
   return (
     <Container>
