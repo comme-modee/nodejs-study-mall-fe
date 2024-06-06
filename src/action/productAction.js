8d5eb7df9feda93c6ab31b3e0c7bc21f57103a23
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-06 19:42:04 +0900

    Update productAction.js

--- src/action/productAction.js
+++ src/action/productAction.js
@@ -11,7 +11,6 @@ const getProductList = (query) => async (dispatch) => {
     });
     if(res.status === 200) {
       dispatch({type:types.PRODUCT_GET_SUCCESS, payload: res.data});
-      console.log('product data', res.data.data)
     } else if (res.status === 400) {
       throw new Error('there is no data')
     }
@@ -25,13 +24,12 @@ const getProductList = (query) => async (dispatch) => {
 const getProductDetail = (id) => async (dispatch) => {};
 
 const createProduct = (formData) => async (dispatch) => {
-  console.log(formData)
   try {
     dispatch({type:types.PRODUCT_CREATE_REQUEST});
     const res = await api.post('/product', formData);
-    console.log('createProduct', res)
     if(res !== 200) throw new Error(res.error);
     dispatch({type:types.PRODUCT_CREATE_SUCCESS});
+    getProductList();
     dispatch(commonUiActions.showToastMessage('상품 생성 완료', 'success'));
   } catch (error) {
     dispatch({type:types.PRODUCT_CREATE_FAIL, payload: error.error});
