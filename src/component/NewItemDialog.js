441722ede8297e0d919b1c5a45b4d77aa5b6b681
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-07 01:41:45 +0900

    Update NewItemDialog.js

--- src/component/NewItemDialog.js
+++ src/component/NewItemDialog.js
@@ -19,9 +19,9 @@ const InitialFormData = {
   price: 0,
 };
 const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
-  const selectedProduct = useSelector((state) => state.product.selectedProduct);
+  const { selectedProduct } = useSelector((state) => state.product);
   const { error } = useSelector((state) => state.product);
-  const [formData, setFormData] = useState(
+  const [ formData, setFormData ] = useState(
     mode === "new" ? { ...InitialFormData } : selectedProduct
   );
   const [ stock, setStock ] = useState([]);
@@ -31,7 +31,6 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
     //모든걸 초기화시키고;
     // 다이얼로그 닫아주기
     setFormData(InitialFormData);
-    setStock([])
     setShowDialog(false);
   };
 
@@ -52,7 +51,6 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
       dispatch(productActions.createProduct({...formData, stock: totalStock}));
       dispatch(productActions.getProductList());
       setFormData(InitialFormData);
-      setStock([])
       setShowDialog(false);
     } else {
       // 상품 수정하기
@@ -116,8 +114,16 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
     if (showDialog) {
       if (mode === "edit") {
         // 선택된 데이터값 불러오기 (재고 형태 객체에서 어레이로 바꾸기)
+        setFormData(selectedProduct)
+        const stockArray = Object.keys(selectedProduct.stock).map((size) => [
+          size,
+          selectedProduct.stock[size]
+        ]);
+        setStock(stockArray)
       } else {
         // 초기화된 값 불러오기
+        setFormData({...InitialFormData});
+        setStock([])
       }
     }
   }, [showDialog]);
