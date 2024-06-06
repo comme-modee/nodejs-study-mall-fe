9ffeb288e3078e8b749d69a3e974bca34f148b15
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-06 20:03:35 +0900

    Update ProductAll.js

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -8,14 +8,26 @@ import { commonUiActions } from "../action/commonUiAction";
 
 const ProductAll = () => {
   const dispatch = useDispatch();
+  const { productList, totalPage } = useSelector((state) => state.product);
+  const [ query, setQuery ] = useSearchParams();
+  const [ searchQuery, setSearchQuery ] = useState({
+    page: query.get("page") || 1,
+    name: query.get("name") || "",
+  }); //검색 조건들을 저장하는 객체
   const error = useSelector((state) => state.product.error);
+    
   // 처음 로딩하면 상품리스트 불러오기
+  useEffect(()=>{
+    dispatch(productActions.getProductList({ ...searchQuery }))
+  },[query])
 
   return (
     <Container>
       <Row>
         <Col md={3} sm={12}>
-          <ProductCard />
+          {productList && productList.map((item) => {
+              <ProductCard title={item.name} img={item.image} price={item.price}/>
+          })}
         </Col>
       </Row>
     </Container>
