3af0246aaabe98636529e5a3f881788b5f5cb138
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-07 01:36:29 +0900

    Update AdminProduct.js

--- src/page/AdminProduct.js
+++ src/page/AdminProduct.js
@@ -57,6 +57,9 @@ const AdminProduct = () => {
   const openEditForm = (product) => {
     //edit모드로 설정하고
     // 아이템 수정다이얼로그 열어주기
+    setMode("edit")
+    setShowDialog(true)
+    dispatch({type: types.SET_SELECTED_PRODUCT, payload: product})
   };
 
   const handleClickNewItem = () => {
