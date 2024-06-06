ba207238573148efa2f301b757c983df0cb2694c
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-06 20:55:21 +0900

    Update ProductAll.js

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -9,23 +9,28 @@ import { useSearchParams } from "react-router-dom";
 const ProductAll = () => {
   const dispatch = useDispatch();
   const [ query, setQuery ] = useSearchParams();
+  const [ searchQuery, setSearchQuery ] = useState({
+    page: query.get("page") || 1,
+    name: query.get("name") || "",
+  }); //검색 조건들을 저장하는 객체
+
   const { productList, totalPage } = useSelector((state) => state.product);
   const error = useSelector((state) => state.product.error);
     
   // 처음 로딩하면 상품리스트 불러오기
   useEffect(()=>{
-    dispatch(productActions.getProductList(query))
-    console.log(query)
+    dispatch(productActions.getProductList({ ...searchQuery }))
   },[query])
+
 
   return (
     <Container>
       <Row>
-        <Col md={3} sm={12}>
           {productList && productList.map((item) => (
-              <ProductCard title={item.name} image={item.image} price={item.price}/>
+            <Col md={3} sm={12}>
+                <ProductCard title={item.name} image={item.image} price={item.price}/>
+            </Col>
           ))}
-        </Col>
       </Row>
     </Container>
   );
