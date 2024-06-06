fa5c0d70cfb709284a71cdb4a2f9fb633c66fef5
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-06 15:33:39 +0900

    Update SearchBox.js

--- src/component/SearchBox.js
+++ src/component/SearchBox.js
@@ -5,10 +5,12 @@ import { useSearchParams } from "react-router-dom";
 
 const SearchBox = ({ searchQuery, setSearchQuery, placeholder, field }) => {
   const [query] = useSearchParams();
-  const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState(query.get(field) || "");
 
-  const onCheckEnter = () => {
-      setSearchQuery({ ...searchQuery, page: 1, [field]: keyword });
+  const onCheckEnter = (event) => {
+    if (event.key === "Enter") {
+      setSearchQuery({ ...searchQuery, page: 1, [field]: event.target.value });
+    }
   };
   return (
     <div className="search-box">
@@ -16,8 +18,8 @@ const SearchBox = ({ searchQuery, setSearchQuery, placeholder, field }) => {
       <input
         type="text"
         placeholder={placeholder}
-        onKeyUp={onCheckEnter}
-        onChange={(e) => setKeyword(e.target.value)}
+        onKeyPress={onCheckEnter}
+        onChange={(event) => setKeyword(event.target.value)}
         value={keyword}
       />
     </div>
