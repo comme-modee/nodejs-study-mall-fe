7e3e3669756852aa7d938f4eb9f59ea97f4ec4c9
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-09 01:42:21 +0900

    Update cartAction.js

--- src/action/cartAction.js
+++ src/action/cartAction.js
@@ -8,7 +8,7 @@ const addToCart =
       dispatch({type: types.ADD_TO_CART_REQUEST});
       const res = await api.post('/cart', { productId: id, size, qty: 1 });
       if(res.status === 200) {
-        dispatch({type: types.ADD_TO_CART_SUCCESS, payload: res.data.cartItemQty});
+        dispatch({type: types.ADD_TO_CART_SUCCESS, payload: res.data});
         dispatch(commonUiActions.showToastMessage('카트에 상품이 추가되었습니다.', 'success'));
       } else if(res.status === 400) {
         throw new Error('카트 추가에 실패했습니다.')
