b14744f98b534c60b2305d9a0dd7f1fad2fe031c
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-09 00:29:40 +0900

    Update productReducer.js

--- src/reducer/productReducer.js
+++ src/reducer/productReducer.js
@@ -13,12 +13,13 @@ function productReducer(state = initialState, action) {
     case types.PRODUCT_CREATE_REQUEST:
     case types.PRODUCT_GET_REQUEST:
     case types.PRODUCT_EDIT_REQUEST:
-    // case types.PRODUCT_DELETE_REQUEST:
+    case types.PRODUCT_DELETE_REQUEST:
+    case types.GET_PRODUCT_DETAIL_REQUEST:
       return {...state, loading: true}
 
     case types.PRODUCT_CREATE_SUCCESS:
     case types.PRODUCT_EDIT_SUCCESS:
-    // case types.PRODUCT_DELETE_SUCCESS:
+    case types.PRODUCT_DELETE_SUCCESS:
       return {...state, loading: false, error: ''}
 
     case types.PRODUCT_GET_SUCCESS:
@@ -27,12 +28,16 @@ function productReducer(state = initialState, action) {
     case types.PRODUCT_CREATE_FAIL:
     case types.PRODUCT_GET_FAIL:
     case types.PRODUCT_EDIT_FAIL:
-    // case types.PRODUCT_DELETE_FAIL:
+    case types.PRODUCT_DELETE_FAIL:
+    case types.GET_PRODUCT_DETAIL_FAIL:
       return {...state, loading: false, error:payload}
 
     case types.SET_SELECTED_PRODUCT:
       return {...state, selectedProduct:payload}
-      
+
+    case types.GET_PRODUCT_DETAIL_SUCCESS:
+      return {...state, loading: false, selectedProduct:payload}
+
     default:
       return state;
   }
