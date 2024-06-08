32c405538c1b8835bae6e5000d603b3d5337a4b9
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-09 00:50:05 +0900

    Update ProductDetail.js

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -38,6 +38,19 @@ const ProductDetail = () => {
     dispatch(productActions.getProductDetail(id));
   }, [id]);
 
+  if (loading || !selectedProduct)
+    return (
+      <ColorRing
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+      />
+    );
+
   return (
     <Container className="product-detail-card">
       <Row>
