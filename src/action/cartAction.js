b8fcaf5253e9a089825bc321f6bb8d9d75308bfd
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-09 01:12:41 +0900

    Update cartAction.js

--- src/action/cartAction.js
+++ src/action/cartAction.js
@@ -3,7 +3,21 @@ import * as types from "../constants/cart.constants";
 import { commonUiActions } from "../action/commonUiAction";
 const addToCart =
   ({ id, size }) =>
-  async (dispatch) => {};
+  async (dispatch) => {
+    try {
+      dispatch({type: types.ADD_TO_CART_REQUEST});
+      const res = await api.post('/cart', { productId: id, size, qty: 1 });
+      if(res.status === 200) {
+        dispatch({type: types.ADD_TO_CART_SUCCESS, payload: res.data});
+        dispatch(commonUiActions.showToastMessage('카트 추가 완료', 'success'));
+      } else if(res.status === 400) {
+        throw new Error('카트 추가에 실패했습니다.')
+      }
+    } catch (error) {
+      dispatch({type: types.ADD_TO_CART_FAIL});
+      dispatch(commonUiActions.showToastMessage(error.error, 'error'));
+    }
+  };
 
 const getCartList = () => async (dispatch) => {};
 const deleteCartItem = (id) => async (dispatch) => {};
