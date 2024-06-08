872bbf88fefac67f9eb996617f3f0f732d1dcee3
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-09 00:30:56 +0900

    Update ProductCard.js

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -2,19 +2,20 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { currencyFormat } from "../utils/number";
 
-const ProductCard = ({ title, price, image }) => {
+const ProductCard = ({ item }) => {
   const navigate = useNavigate();
   const showProduct = (id) => {
     // 상품 디테일 페이지로 가기
+    navigate(`/product/${id}`);
   };
   return (
-    <div className="card" onClick={() => showProduct("hard_code")}>
+    <div className="card" onClick={() => showProduct(item._id)}>
       <img
-        src={image}
-        alt={title}
+        src={item.image}
+        alt={item.title}
       />
-      <div>{title}</div>
-      <div>₩ {price}</div>
+      <div>{item.title}</div>
+      <div>₩ {item.price}</div>
     </div>
   );
 };
