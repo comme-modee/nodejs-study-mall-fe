352e557da440ffc796cdaedf3596a93d0e5aa0d3
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-09 21:08:44 +0900

    Update cartReducer.js

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -16,18 +16,50 @@ const initialState = {
 function cartReducer(state = initialState, action) {
   const { type, payload } = action;
   switch(type) {
+    case LOGOUT: {
+      return { ...state, cartItemQty: 0 };
+    }
     case types.ADD_TO_CART_REQUEST:
     case types.GET_CART_LIST_REQUEST:
     case types.DELETE_CART_ITEM_REQUEST:
+    case types.UPDATE_CART_ITEM_REQUEST:
+    case types.GET_CART_QTY_REQUEST:
       return {...state, loading: true}
+
     case types.ADD_TO_CART_SUCCESS:
     case types.DELETE_CART_ITEM_SUCCESS:
       return { ...state, loading: false, cartItemQty: payload };
+
+    case types.UPDATE_CART_ITEM_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        cartList: payload,
+        totalPrice: payload.reduce(
+          (total, item) => (total += item.productId.price * item.qty),
+          0
+        ),
+      };
+
     case types.GET_CART_LIST_SUCCESS:
-      return {...state, cartList: payload, cartItemQty: payload.length, totalPrice: payload.reduce((total, item) => (total += item.productId.price * item.qty), 0)}
+      return {
+        ...state,
+        loading: false,
+        cartList: payload,
+        totalPrice: payload.reduce(
+          (total, item) => (total += item.productId.price * item.qty),
+          0
+        ),
+      };
+
+    case types.GET_CART_QTY_SUCCESS:
+      return { ...state, cartItemQty: payload };
+      
     case types.ADD_TO_CART_FAIL:
     case types.GET_CART_LIST_FAIL:
     case types.DELETE_CART_ITEM_FAIL:
+    case types.UPDATE_CART_ITEM_FAIL:
+    case types.GET_CART_QTY_FAIL:
       return {...state, loading:false, error: payload}
 
     default:
