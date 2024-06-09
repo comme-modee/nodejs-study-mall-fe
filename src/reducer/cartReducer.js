a19156ae0c6d52914e9202be5414283bb7b83718
Modee <144014486+comme-modee@users.noreply.github.com>
2024-06-09 20:47:07 +0900

    Update cartReducer.js

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -18,13 +18,16 @@ function cartReducer(state = initialState, action) {
   switch(type) {
     case types.ADD_TO_CART_REQUEST:
     case types.GET_CART_LIST_REQUEST:
+    case types.DELETE_CART_ITEM_REQUEST:
       return {...state, loading: true}
     case types.ADD_TO_CART_SUCCESS:
-      return {...state, cartItemQty: payload}
+    case types.DELETE_CART_ITEM_SUCCESS:
+      return { ...state, loading: false, cartItemQty: payload };
     case types.GET_CART_LIST_SUCCESS:
-      return {...state, cartList: payload, totalPrice: payload.reduce((total, item) => (total += item.productId.price * item.qty), 0)}
+      return {...state, cartList: payload, cartItemQty: payload.length, totalPrice: payload.reduce((total, item) => (total += item.productId.price * item.qty), 0)}
     case types.ADD_TO_CART_FAIL:
     case types.GET_CART_LIST_FAIL:
+    case types.DELETE_CART_ITEM_FAIL:
       return {...state, loading:false, error: payload}
 
     default:
